dd5d41a59313084a24ad10b24a3d91edf6b0699a
Zer0kira <jpablomejia01@gmail.com>
2020-02-06 15:06:53 -0500

    Init project
    Creando estructura del proyecto

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include 'views/partials/head.php'; ?>
+<?php include 'views/partials/menu.php'; ?>
+
+<div class="container">
+	<div class="jumbotron">
+		<div class="container">
+			<h1>Login con PHP</h1>
+			<p>Curso de login</p>
+			<p>
+				<a href="login.php" class="btn btn-primary btn-lg">Login</a>
+			</p>
+		</div>
+	</div>
+</div>
+
+<?php include 'views/partials/footer.php'; ?>
